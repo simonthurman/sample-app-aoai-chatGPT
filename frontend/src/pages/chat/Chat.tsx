ded1a858873f4b86ac8d56df5d75adea146c60fa
Simon Thurman <simonthurman@hotmail.com>
2023-08-31 13:54:28 +0100

    Update Chat.tsx
    update auth

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -85,14 +85,9 @@ const Chat = () => {
         }, 500);
     }
     
+
     const getUserInfoList = async () => {
-        const userInfoList = await getUserInfo();
-        if (userInfoList.length === 0 && window.location.hostname !== "127.0.0.1") {
-            setShowAuthMessage(true);
-        }
-        else {
-            setShowAuthMessage(false);
-        }
+        setShowAuthMessage(false);
     }
 
     const makeApiRequestWithoutCosmosDB = async (question: string, conversationId?: string) => {
